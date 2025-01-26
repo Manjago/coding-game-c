911d239c39a3ee55a32a34dc20b00300d70b0a83
Kirill Temnenkov <temnenkov@yandex.ru>
2025-01-26 22:03:25 +0300

    new verison

--- comp.sh
+++ comp.sh
@@ -1,2 +1,2 @@
 # gcc -O0 -g -Wall -Wextra -Werror -pedantic-errors -Wpedantic -Wcast-align -Wcast-qual -Wconversion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wlogical-op -Wredundant-decls -Wsign-conversion code-vs-zombies.c -lpthread -ldl -lm -o code-vs-zombies 
-gcc -O0 -g -Wall -Wextra -Werror -pedantic-errors -Wpedantic -Wcast-align -Wcast-qual -Wconversion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wlogical-op -Wredundant-decls -Wsign-conversion codingame-sponsored-contest.c -lpthread -ldl -lm -o out
+gcc -O0 -g -Wall -Wextra -Werror -pedantic-errors -Wpedantic -Wcast-align -Wcast-qual -Wconversion -Wduplicated-branches -Wduplicated-cond -Wfloat-equal -Wlogical-op -Wredundant-decls -Wsign-conversion maze.c -lpthread -ldl -lm -o out
---
+++ maze.c
@@ -0,0 +1,206 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int width;
+int height;
+
+
+int up_index(int j) {
+  int new_value = j - 1;
+  if (new_value < 0) {
+    new_value += height;
+  }
+  return new_value;
+}
+
+int down_index(int j) {
+  int new_value = j + 1;
+  if (new_value >= height) {
+    new_value -= height;
+  }
+  return new_value;
+}
+
+int left_index(int i) {
+  int new_value = i - 1;
+  if (new_value < 0) {
+    new_value += width;
+  }
+  return new_value;
+}
+
+int right_index(int i) {
+  int new_value = i + 1;
+  if (new_value >= width) {
+    new_value -= width;
+  }
+  return new_value;
+}
+
+enum constraints { max_width = 35, max_height = 35, max_players = 10 };
+
+struct point {
+  int x, y;
+};
+
+typedef struct point Point;
+
+bool point_equals(const struct point a, const struct point b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+Point point_up(const Point point) {
+  Point result = point;
+  result.y = up_index(result.y);
+  return result;
+}
+
+Point point_down(const Point point) {
+  Point result = point;
+  result.y = down_index(result.y);
+  return result;
+}
+
+Point point_left(const Point point) {
+  Point result = point;
+  result.x = left_index(result.x);
+  return result;
+}
+
+Point point_right(const Point point) {
+  Point result = point;
+  result.x = right_index(result.x);
+  return result;
+}
+
+enum cell_type { unknown = 0, wall = 1, space = 2, explored = 4 };
+
+typedef enum cell_type CellType;
+
+CellType from_char(const char raw_status) {
+  assert(raw_status == '#' || raw_status == '_');
+  return (raw_status == '#') ? wall + explored : space + explored;
+}
+
+CellType grid[max_height][max_width] = {0};
+
+void setCellType(Point point, CellType value) {
+  grid[point.y][point.x] = grid[point.y][point.x] | value;
+}
+
+struct game_state {
+  Point monsters[max_players - 1];
+  int monsters_count;
+  Point explorer;
+};
+
+typedef struct game_state GameState;
+
+int index_of_monster(const GameState *game_state, const Point point) {
+  for (int i = 0; i < game_state->monsters_count; ++i) {
+    const struct point monster = game_state ->monsters[i];
+    if (point_equals(monster, point)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void dump_grid(const GameState *game_state) {
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; ++j) {
+      const Point point = {j, i};
+      const int monster_index = index_of_monster(game_state, point);
+      if (monster_index + 1) {
+        fprintf(stderr, "%d", monster_index);
+      } else {
+        const CellType cell_type = grid[i][j];
+        if (cell_type == unknown) {
+          fprintf(stderr, "%c", '?');
+        } else if (cell_type & wall) {
+          fprintf(stderr, "%c", '#');
+        } else if (cell_type & explored) {
+          fprintf(stderr, "%c", '.');
+        } else { // unexplored, but space
+          fprintf(stderr, "%c", ',');
+        }
+      }
+    }
+    fprintf(stderr, "\n");
+  }
+}
+
+int main() {
+  scanf("%d", &width);
+  assert(width > 0 && width <= max_width);
+  scanf("%d", &height);
+  assert(height > 0 && height <= max_height);
+  scanf("%d", &players_count);
+  int players_count;
+  assert(players_count > 0 && players_count <= max_players);
+  fgetc(stdin);
+
+  unsigned int seed = (unsigned int)time(NULL);
+  fprintf(stderr, "ver 1.0.0, seed = %u\n", seed);
+  srand(seed);
+
+  fprintf(stderr, "width %d, height %d, players count %d\n", width, height,
+          players_count);
+
+  // game loop
+  int turn_num = 0;
+  while (++turn_num) {
+    char up_status[2];
+    scanf("%[^\n]", up_status);
+    fgetc(stdin);
+    char right_status[2];
+    scanf("%[^\n]", right_status);
+    fgetc(stdin);
+    char down_status[2];
+    scanf("%[^\n]", down_status);
+    fgetc(stdin);
+    char left_status[2];
+    scanf("%[^\n]", left_status);
+
+    fprintf(stderr, "up:%c, right:%c, down:%c, left:%c\n", up_status[0], right_status[0],
+            down_status[0], left_status[0]);
+
+    GameState game_state;
+    game_state.monsters_count = players_count - 1;
+
+    for (int i = 0; i < players_count; i++) {
+      int x;
+      int y;
+      scanf("%d%d", &x, &y);
+      fgetc(stdin);
+      const int x_mod = x % width;
+      const int y_mod = y % height;
+      fprintf(stderr, "%d: %d %d -> %d %d\n", i, x, y, x_mod, y_mod);
+      const Point pos = {x_mod, y_mod};
+      setCellType(pos, space);
+      if (i == players_count - 1) {
+        game_state.explorer = pos;
+        setCellType(point_up(pos), from_char(up_status[0]));
+        setCellType(point_down(pos), from_char(down_status[0]));
+        setCellType(point_right(pos), from_char(right_status[0]));
+        setCellType(point_left(pos), from_char(left_status[0]));
+      } else {
+        game_state.monsters[i] = pos;
+      }
+    }
+
+    dump_grid(players_count);
+    const struct point me = players[players_count - 1];
+    const char move = do_move(me);
+    fprintf(stderr, "turn %d, move ", turn_num);
+    decode_move(move);
+    fprintf(stderr, "\n");
+    printf("%c\n", move);
+  }
+
+  return 0;
+}
